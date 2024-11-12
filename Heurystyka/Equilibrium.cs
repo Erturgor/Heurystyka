e059b2a16df5eef8e8b32bc1fb3b6abfb63f57a3
Erturgor <96728263+Erturgor@users.noreply.github.com>
2024-11-12 10:42:38 +0100

    Add project files.

---
+++ Heurystyka/Equilibrium.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heurystyka
+{
+    public class Equilibrium : IOptimizationAlogirthm
+    {
+        public string Name {get; set;}
+        public double[] XBest { get; set; }
+        public double FBest { get; set; }
+        public int NumberOfEvaluationFitnessFunction { get; set; }
+
+        public double Solve(Func<double[], double>,)
+        {
+            readfile();
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Heurystyka/IOptimizationAlogirthm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heurystyka
+{
+    public interface IOptimizationAlogirthm
+    {
+        string Name { get; set; }
+        //rozwiazanie
+        //czy jest plik wczytanie pliku
+        //jesli nie od poczatku
+        //zwraca wartosc funkcji celu dla znalezionego rozwiazania
+        double Solve();
+        double[] XBest {  get; set; }
+        double FBest { get; set; }
+        //zwraca liczbę wywołan funkcji dopasowania
+        int NumberOfEvaluationFitnessFunction { get; set; }
+
+    }
+}
