7f249531842500b017b0ffa3c091197cdab509ea
Erturgor <96728263+Erturgor@users.noreply.github.com>
2024-11-19 15:00:20 +0100

    testowanie

--- Heurystyka/Program.cs
+++ Heurystyka/Program.cs
@@ -3,7 +3,7 @@ using Heurystyka;
 using System.Numerics;
 using System.Runtime.Intrinsics;
 //var fun = Funkcje.Sphere;
-var fun = Funkcje.Beale;
+var fun = Funkcje.Sphere;
 //var fun = Funkcje.Rosenbrock;
 Equilibrium equilibrium = new Equilibrium();
 equilibrium.fit(fun,N:50, i:10,d:5, Max:4.5, Min:-4.5);
@@ -11,10 +11,16 @@ var wartosc = equilibrium.Solve();
 Console.WriteLine(wartosc.ToString());
 Console.WriteLine("[{0}]", String.Join(" | ",equilibrium.XBest));
 Console.WriteLine();
-ArtificialBeeColony artificialBee = new ArtificialBeeColony();
-artificialBee.fit(fun, N: 10, i: 10, d:2 , Max: 3, Min: -15);
-var wartosc2 = artificialBee.Solve();
-Console.WriteLine(wartosc2.ToString());
-Console.WriteLine(fun(artificialBee.XBest));
-Console.WriteLine("[{0}]", String.Join(" | ", artificialBee.XBest));
-Console.WriteLine();
+
+for (int i = 10; i < 100; i+=10)
+{
+    ArtificialBeeColony artificialBee = new ArtificialBeeColony();
+    artificialBee.fit(fun, N: 40, i: i, d: 50, Max: 5, Min: -5);
+    var wartosc2 = artificialBee.Solve();
+    Console.WriteLine(wartosc2.ToString());
+    Console.WriteLine(fun(artificialBee.XBest));
+    Console.WriteLine("[{0}]", String.Join(" | ", artificialBee.XBest));
+    Console.WriteLine();
+}
+
+
